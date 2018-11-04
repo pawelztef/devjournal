78385ad6b2e83ae6256ba9d3b48905893aaef3ae
pawelztef <pawelztef@gmail.com>
2018-11-04 16:04:06 +0000

    menu finished

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
   toggleMenu();
+
 });
-
-
 
 let toggleMenu = function() {
 
@@ -16,25 +15,19 @@ let toggleMenu = function() {
     closeBtn.css('display', 'initial');
   });
 
-  closeBtn.on('click', function(e) {
-    e.preventDefault();
+  closeBtn.on('click', function() {
     target.css('width', '0%'); 
     openBtn.css('visibility', 'initial');
     closeBtn.css('display', 'none');
   });
 
   $('.menu-list a').on('click', function(e) {
-    e.preventDefault();
     let link = $(this);
     if(link.hasClass('hover-active')) {
     } else {
       link.addClass('hover-active');
       $('a').not(this).removeClass('hover-active');
     }
-    window.setTimeout(function() {
-      target.css('width', '0%'); 
-      openBtn.css('visibility', 'initial');
-      closeBtn.css('display', 'none');
-    }, 100);
   });
 }
+
