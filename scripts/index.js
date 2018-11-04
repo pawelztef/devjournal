82aa07665ce9f6498a7acd0317a3f55fbc26a7a3
pawelztef <pawelztef@gmail.com>
2018-11-04 13:39:35 +0000

    jquery swap hover function

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
   openNav();
   closeNav();
+  swapHover();
 });
 
 let openNav = function() {
-  let openBtn = $(".burger");
-  let target = $(".overlay");
+  let openBtn = $('.burger');
+  let target = $('.overlay');
   openBtn.on('click', function() {
     target.css('width', '100%'); 
     openBtn.css('visibility', 'hidden');
   });
 }
 let closeNav = function() {
-  let closeBtn = $(".closeBtn");
-  let openBtn = $(".burger");
-  let target = $(".overlay");
+  let closeBtn = $('.closeBtn');
+  let openBtn = $('.burger');
+  let target = $('.overlay');
   closeBtn.on('click', function() {
     target.css('width', '0%'); 
     openBtn.css('visibility', 'initial');
+  });
+}
+let swapHover = function() {
+  $('.menu-list a').on('click', function(e) {
+    let link = $(this);
+    if(link.hasClass('hover-active')) {
+      console.log('one');
+      return true;
+    } else {
+      link.addClass('hover-active');
+      $('a').not(this).removeClass('hover-active');
+      console.log('two');
+      console.log($('a').not(this));
+      return true;
+    }
   });
 }
